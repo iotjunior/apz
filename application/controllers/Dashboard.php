21dc69e9315f3b4ac964a540f5980305c8a1671f
IoT Inventor <iotinventor@gmail.com>
2022-02-03 14:45:26 +0700

    add remote address to get ip public

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,17 +1,41 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+function get_client_ip() {
+  // $ipaddress = '';
+  //whether ip is from share internet
+  if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+  {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+  }
+  //whether ip is from proxy
+  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+  {
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+  //whether ip is from remote address
+  else
+  {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+  }
+  return $ip_address;
+  // return $ipaddress;
+}
+
 class Dashboard extends MY_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('project_model');
     $this->load->model('publication_model');
     $this->load->model('boards_model');
+    
 	}
 
   public function index() {
     $this->guard();
+    
     if ($this->input->server('REQUEST_METHOD') === 'GET') {
+      echo get_client_ip();
       // get my projects
       $data["projects"] = $this->project_model->latest($this->session->userdata('email'), 6);
       // get my publications
